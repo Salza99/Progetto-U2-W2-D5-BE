e6dd22742e0497d36b0c128fbc6f14757be3dfdc
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-10 10:11:18 +0100

    set up entities with their relation

--- src/main/java/DavideSalzani/ProgettoU2W2D5BE/users/User.java
+++ src/main/java/DavideSalzani/ProgettoU2W2D5BE/users/User.java
@@ -1,4 +1,27 @@
 package DavideSalzani.ProgettoU2W2D5BE.users;
 
+import DavideSalzani.ProgettoU2W2D5BE.devices.Device;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String name;
+    private String surname;
+    private String Email;
+    @OneToMany(mappedBy = "assignedTo")
+    @JsonIgnore
+    private List<Device> assignedCompanyDevices;
 }
